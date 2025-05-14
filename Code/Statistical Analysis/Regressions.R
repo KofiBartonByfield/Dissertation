2b693ae8a3dd9923ade753e80eaa5d3ab4b41d63
Kofi Barton-Byfield <kbartonbyfield@gmail.com>
2025-05-14 14:00:20 +0100

    Create Regressions.R

---
+++ Code/Statistical Analysis/Regressions.R
@@ -0,0 +1,48 @@
+library(dplyr)
+library(fixest)
+library(MASS)
+library(car)
+library(stargazer)
+
+
+dataset <- read.csv('Data/Data Sets/cleaned_dataset.csv')
+
+merseyside <- dataset %>% filter(PoliceDept == "Merseyside")
+london <- dataset %>% filter(PoliceDept == "London")
+
+head(merseyside)
+head(london)
+
+
+
+
+
+
+
+
+
+
+london_regression_1.1 <- fenegbin(StopCount ~ 
+                                IncomeDomainScore_z + 
+                                MeanHousePrice_z + 
+                                CrimeSum_z + 
+                                BAME_z + 
+                                DrugCrimeSum_z | Borough,
+                              data = london)
+
+london_regression_1.2 <- fenegbin(StopCount ~ 
+                                CrimeSum_z + 
+                                DrugCrimeSum_z | Borough,
+                              data = london)
+
+
+
+
+etable(london_regression_1.1, london_regression_1.2)
+
+
+
+
+
+
+
