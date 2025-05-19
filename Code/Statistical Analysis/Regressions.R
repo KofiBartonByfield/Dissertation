1b650162ed75ff68f26416e9494839f51e3966e0
Kofi Barton-Byfield <kbartonbyfield@gmail.com>
2025-05-19 16:24:42 +0100

    final regression tables

--- Code/Statistical Analysis/Regressions.R
+++ Code/Statistical Analysis/Regressions.R
@@ -20,26 +20,56 @@ head(london)
 
 
 
-
-
 london_regression_1.1 <- fenegbin(StopCount ~ 
+                                    gini + 
+                                    IncomeDomainScore_z + 
+                                    MeanHousePrice_z + 
+                                    CrimeSum_z + 
+                                    EthnicMinority_z + 
+                                    DrugCrimeSum_z,
+                                  data = london)
+
+london_regression_1.2 <- fenegbin(StopCount ~ 
                                 gini + 
                                 IncomeDomainScore_z + 
                                 MeanHousePrice_z + 
                                 CrimeSum_z + 
-                                BAME_z + 
-                                DrugCrimeSum_z | Borough,
-                              data = london)
-
-london_regression_1.2 <- fenegbin(StopCount ~ 
-                                CrimeSum_z + 
+                                EthnicMinority_z + 
                                 DrugCrimeSum_z | Borough,
                               data = london)
 
 
+london_regression_1.3 <- fenegbin(StopCount ~ 
+                                    gini + 
+                                    IncomeDomainScore_z + 
+                                    MeanHousePrice_z + 
+                                    CrimeSum_z + 
+                                    EthnicMinority_z + 
+                                    DrugCrimeSum_z +
+                                    gini*EthnicMinority_z| Borough,
+                                  data = london)
 
 
-etable(london_regression_1.1, london_regression_1.2)
+london_regression_1.4 <- fenegbin(StopCount ~ 
+                                    gini + 
+                                    IncomeDomainScore_z + 
+                                    MeanHousePrice_z + 
+                                    CrimeSum_z + 
+                                    EthnicMinority_z + 
+                                    DrugCrimeSum_z +
+                                    IncomeDomainScore_z*EthnicMinority_z| Borough,
+                                  data = london)
+
+
+
+
+
+
+etable(london_regression_1.1, 
+       london_regression_1.2,
+       london_regression_1.3, 
+       london_regression_1.4)
+
 
 
 
