28b09f21b3cac99acd2d1c104f7b7ddf3cfadb57
Kofi Barton-Byfield <kbartonbyfield@gmail.com>
2025-05-19 16:25:43 +0100

    regression tables

--- Code/Statistical Analysis/Regressions.R
+++ Code/Statistical Analysis/Regressions.R
@@ -36,6 +36,7 @@ london_regression_1.2 <- fenegbin(StopCount ~
                                 CrimeSum_z + 
                                 EthnicMinority_z + 
                                 DrugCrimeSum_z | Borough,
+                                cluster = "Borough",                                
                               data = london)
 
 
@@ -47,6 +48,7 @@ london_regression_1.3 <- fenegbin(StopCount ~
                                     EthnicMinority_z + 
                                     DrugCrimeSum_z +
                                     gini*EthnicMinority_z| Borough,
+                                    cluster = "Borough",
                                   data = london)
 
 
@@ -58,6 +60,7 @@ london_regression_1.4 <- fenegbin(StopCount ~
                                     EthnicMinority_z + 
                                     DrugCrimeSum_z +
                                     IncomeDomainScore_z*EthnicMinority_z| Borough,
+                                    cluster = "Borough",
                                   data = london)
 
 
@@ -69,6 +72,130 @@ etable(london_regression_1.1,
        london_regression_1.2,
        london_regression_1.3, 
        london_regression_1.4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+merseyside_regression_1.1 <- fenegbin(StopCount ~ 
+                                    gini + 
+                                    IncomeDomainScore_z + 
+                                    MeanHousePrice_z + 
+                                    CrimeSum_z + 
+                                    EthnicMinority_z + 
+                                    DrugCrimeSum_z,
+                                  data = merseyside)
+
+merseyside_regression_1.2 <- fenegbin(StopCount ~ 
+                                    gini + 
+                                    IncomeDomainScore_z + 
+                                    MeanHousePrice_z + 
+                                    CrimeSum_z + 
+                                    EthnicMinority_z + 
+                                    DrugCrimeSum_z | Borough,
+                                  cluster = "Borough",                                
+                                  data = merseyside)
+
+
+merseyside_regression_1.3 <- fenegbin(StopCount ~ 
+                                    gini + 
+                                    IncomeDomainScore_z + 
+                                    MeanHousePrice_z + 
+                                    CrimeSum_z + 
+                                    EthnicMinority_z + 
+                                    DrugCrimeSum_z +
+                                    gini*EthnicMinority_z| Borough,
+                                  cluster = "Borough",
+                                  data = merseyside)
+
+
+merseyside_regression_1.4 <- fenegbin(StopCount ~ 
+                                    gini + 
+                                    IncomeDomainScore_z + 
+                                    MeanHousePrice_z + 
+                                    CrimeSum_z + 
+                                    EthnicMinority_z + 
+                                    DrugCrimeSum_z +
+                                    IncomeDomainScore_z*EthnicMinority_z| Borough,
+                                  cluster = "Borough",
+                                  data = merseyside)
+
+
+
+
+
+
+etable(merseyside_regression_1.1, 
+       merseyside_regression_1.2,
+       merseyside_regression_1.3, 
+       merseyside_regression_1.4)
+
+
+
+
+
+
+vars <- c(
+  gini = "Gini Coefficient",
+  IncomeDomainScore_z = "Income Deprivation (z)",
+  MeanHousePrice_z = "Mean House Price (z)",
+  CrimeSum_z = "Crime Rate (z)",
+  EthnicMinority_z = "Ethnic Minority (z)",
+  DrugCrimeSum_z = "Drug Crime Rate (z)",
+  `gini x EthnicMinority_z` = "Gini x Ethnic Minority",
+  `IncomeDomainScore_z x EthnicMinority_z` = "Income x Ethnic Minority"
+)
+
+
+
+etable(
+  merseyside_regression_1.1,
+  merseyside_regression_1.2,
+  merseyside_regression_1.3,
+  merseyside_regression_1.4,
+  tex = TRUE,
+  style.tex = style.tex("base"),
+  digits = 3,
+  dict = vars,
+  signif.code = c("***" = 0.001, "**" = 0.01, "*" = 0.05, "." = 0.1, " " = 1)
+)
+
+
+etable(
+  london_regression_1.1,
+  london_regression_1.2,
+  london_regression_1.3,
+  london_regression_1.4,
+  tex = TRUE,
+  style.tex = style.tex("base"),
+  digits = 3,
+  dict = vars,
+  signif.code = c("***" = 0.001, "**" = 0.01, "*" = 0.05, "." = 0.1, " " = 1)
+  )
+
+
+
+
+
+
+
+
+
 
 
 
